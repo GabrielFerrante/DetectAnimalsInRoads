08a642537240d3c5a6684015fff446cb2d5c4883
GabrielFerrante <gabrielsferrante@hotmail.com>
2022-06-24 16:59:47 -0300

    experimento com yolov5

--- YoloV5-Model/divideTrain.py
+++ YoloV5-Model/divideTrain.py
@@ -5,19 +5,21 @@ from shutil import move
 
 
 
-def createTrainAndValid():
+def createTrain():
     # o que o codigo abaixo faz: adiciona o nome e caminho de todos as fotos (.jpg) em um txt
     
-        
+    #Todas as imagens com labels na pasta images do yolov5
     pasta = f'./yolov5/data/images'
-        
+    
     for diretorio, subpastas, arquivos in os.walk(pasta):
         if len(arquivos) == 0:
             print("N tem imagens no images")
         else:
             for arquivo in arquivos:
                 if arquivo.endswith(".jpg") or arquivo.endswith(".txt"):
-                    os.replace(f"{pasta}/{arquivo}", f"{pasta}/train/{arquivo}")   
+                    os.replace(f"{pasta}/{arquivo}", f"{pasta}/train/{arquivo}")  
+                
+                    
     
 
-createTrainAndValid()
+createTrain()
--- YoloV5-Model/divideValid.py
+++ YoloV5-Model/divideValid.py
@@ -5,21 +5,49 @@ from shutil import move
 
 
 
-def createTrainAndValid():
+def createValid():
     # o que o codigo abaixo faz: adiciona o nome e caminho de todos as fotos (.jpg) em um txt
     
         
+    pasta = f'./yolov5/data/images/train'
+    animals = ["anta", "jaguarundi", "loboGuara", "oncaParda", "tamanduaBandeira"]
+    
+
+    for diretorio, subpastas, arquivos in os.walk(pasta):
+        if len(arquivos) == 0:
+            print("N tem imagens no train")
+        if len(getValidImages()) > 0:
+            print("ja tem no valid as imagens")
+        else:
+            for arquivo in arquivos[int(0.8 * len(arquivos)):]:
+                if arquivo.endswith(".jpg"):
+                    os.replace(f"{pasta}/{arquivo}", f"./yolov5/data/images/val/{arquivo}")
+                   
+               
+    
+
+def getValidImages():
+    pastaVal = f'./yolov5/data/images/val'
+    imagesValida = []
+    for diretorio, subpastas, arquivos in os.walk(pastaVal):
+        for arquivo in arquivos:
+            imagesValida.append(arquivo)    
+    return imagesValida
+
+def getValidLabels(imagens):
+
+    print(len(imagens))
     pasta = f'./yolov5/data/images/train'
         
     for diretorio, subpastas, arquivos in os.walk(pasta):
         if len(arquivos) == 0:
             print("N tem imagens no train")
         else:
-            for arquivo in arquivos[int(0.8 * len(arquivos)):]:
-                if arquivo.endswith(".jpg"):
-                    os.replace(f"{pasta}/{arquivo}", f"./yolov5/data/images/val/{arquivo}") 
-                if arquivo.endswith(".txt"):
-                    os.replace(f"{pasta}/{arquivo}", f"./yolov5/data/images/val/{arquivo}") 
-    
+            for arquivo in imagens:
+                arquivo1 = arquivo.replace(".jpg",".txt")
+                os.replace(f"{pasta}/{arquivo1}", f"./yolov5/data/images/val/{arquivo1}") 
+                
 
-createTrainAndValid()
+
+createValid()
+getValidLabels(getValidImages())
