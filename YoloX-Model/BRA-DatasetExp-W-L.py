261dcf9cae6106abb508f20a64be8c2330933fe6
GabrielFerrante <gabrielsferrante@hotmail.com>
2022-10-06 13:03:21 -0300

    Training X

---
+++ YoloX-Model/BRA-DatasetExp-W-L.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+import os
+
+from yolox.exp import Exp as MyExp
+
+
+class Exp(MyExp):
+    def __init__(self):
+        super(Exp, self).__init__()
+        self.depth = 1.0
+        self.width = 1.0
+        self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
+
+        # Define yourself dataset path
+        self.data_dir = "../../BRA-Dataset_COCO_format"
+        self.train_ann = "instances_train2017.json"
+        self.val_ann = "instances_val2017.json"
+
+        self.num_classes = 5
+
+        self.max_epoch = 351
+        self.data_num_workers = 4
+        self.eval_interval = 1
+
+        self.input_size = (416, 416)
+        self.test_size = (416, 416)
